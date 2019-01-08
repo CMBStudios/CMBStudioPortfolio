3041f58fba35cd82b703d7f069322af263557136
CMB Studio <webs.studio@outlook.com>
2019-01-09 03:11:18 +0800

    ip get

--- server/routes/portfolio.routes.js
+++ server/routes/portfolio.routes.js
@@ -5,3 +5,17 @@ const router = new Router();
 
 
 export default router;
+// ip recorder
+fetch("/insertdata", options)
+  .then(res => res.json())
+  .then(
+    (result) => {
+      console.log(window.location.pathname);
+    },
+    // Note: it's important to handle errors here
+    // instead of a catch() block so that we don't swallow
+    // exceptions from actual bugs in components.
+    (error) => {
+      console.log(error);
+    }
+  );
