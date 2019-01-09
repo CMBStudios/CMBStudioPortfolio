99d50c9deca72a4050823a199fd100a3a3f9f130
CMB Studio <webs.studio@outlook.com>
2019-01-09 09:41:11 +0800

    get IP

--- client/index.js
+++ client/index.js
@@ -19,6 +19,22 @@ render(
   mountApp
 );
 
+
+// ip recorder
+fetch("/insertdata", options)
+  .then(res => res.json())
+  .then(
+    (result) => {
+      console.log(window.location.pathname);
+    },
+    // Note: it's important to handle errors here
+    // instead of a catch() block so that we don't swallow
+    // exceptions from actual bugs in components.
+    (error) => {
+      console.log(error);
+    }
+  );
+
 // For hot reloading of react components
 if (module.hot) {
   module.hot.accept('./App', () => {
